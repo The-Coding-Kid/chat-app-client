be2b55864e3d41ed68f01c5d3ae67118178f1d95
aappachi <aravindkrishna.nambi@gmail.com>
2022-07-29 12:06:40 -0700

    bruh meme

--- src/screens/CreatePostScreen.js
+++ src/screens/CreatePostScreen.js
@@ -8,49 +8,136 @@ import {
   Image,
   TouchableWithoutFeedback,
   Keyboard,
+  TouchableOpacity,
 } from "react-native";
 import { TextInput } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import { Button } from "react-native-paper";
+import { Button, Modal, Portal, Provider } from "react-native-paper";
+import DropDownPicker from "react-native-dropdown-picker";
 
 const CreatePOstScreen = ({ navigation: { navigate } }) => {
+  const [item, setItem] = useState("Public");
+  const [visible, setVisible] = React.useState(false);
+  const [visible2, setVisible2] = useState(false);
+
+  const showModal = () => {
+    Keyboard.dismiss();
+    setVisible(true);
+  };
+  const hideModal = () => setVisible(false);
+
+  const showModalForCamera = () => {
+    Keyboard.dismiss();
+    setVisible2(true);
+  };
+  const hideModalForCamera = () => setVisible2(false);
+  const containerStyle = {
+    backgroundColor: "white",
+    height: "65%",
+    marginTop: "90%",
+    padding: 10,
+    borderRadius: 10,
+  };
+
   return (
-    <TouchableWithoutFeedback keyboardShouldPersistTaps="always">
-      <SafeAreaView style={[styles.container, styles.horizontal]}>
-        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-          <Button onPress={() => navigate("HomeTab")}>
-            <Text style={{ color: "black" }}>Cancel</Text>
-          </Button>
-          <Button
+    <Provider>
+      <Portal>
+        <Modal
+          animationType="slide"
+          visible={visible}
+          onDismiss={hideModal}
+          contentContainerStyle={containerStyle}
+          style={{ height: "90%" }}
+        >
+          <Text>Example Modal. Click outside this area to dismiss.</Text>
+        </Modal>
+      </Portal>
+      <Portal>
+        <Modal
+          animationType="slide"
+          visible={visible2}
+          onDismiss={hideModalForCamera}
+          contentContainerStyle={containerStyle}
+          style={{ height: "90%" }}
+        >
+          <Text>Example Modal. Click outside this area to dismiss.</Text>
+        </Modal>
+      </Portal>
+      <TouchableWithoutFeedback keyboardShouldPersistTaps="always">
+        <View style={[styles.container, styles.horizontal]}>
+          <View
             style={{
-              marginTop: 10,
-              backgroundColor: "#3c65a0",
-              alignSelf: "center",
-              marginRight: 10,
-              borderRadius: 100,
+              flexDirection: "row",
+              justifyContent: "space-between",
+              marginTop: 40,
             }}
-            mode="contained"
           >
-            Send
-          </Button>
-        </View>
-        <View style={{ flexDirection: "row" }}>
-          <Image
-            style={styles.tinyLogo}
-            source={{
-              uri: "https://media.brawltime.ninja/brawlers/bonnie/pins/Pin-Phew.png?size=160",
-            }}
-          />
-          <View style={{ marginLeft: 30, marginTop: 30 }}>
-            <TextInput
-              style={{ height: 200 }}
-              multiline
-              placeholder="Something cooking?"
-            ></TextInput>
+            <Button onPress={() => navigate("HomeTab")}>
+              <Text style={{ color: "black" }}>Cancel</Text>
+            </Button>
+            <Button
+              style={{
+                marginTop: 10,
+                backgroundColor: "#3c65a0",
+                alignSelf: "center",
+                marginRight: 10,
+                borderRadius: 100,
+              }}
+              mode="contained"
+            >
+              Send
+            </Button>
+          </View>
+          <View style={{ flexDirection: "row" }}>
+            <Image
+              style={styles.tinyLogo}
+              source={{
+                uri: "https://media.brawltime.ninja/brawlers/bonnie/pins/Pin-Phew.png?size=160",
+              }}
+            />
+            <View style={{ marginLeft: 30, marginTop: 10 }}>
+              <View style={{ flexDirection: "row" }}>
+                <TouchableOpacity
+                  style={{
+                    height: 30,
+                    width: 100,
+                    alignItems: "center",
+                    borderWidth: 1,
+                    justifyContent: "center",
+                    borderRadius: 100,
+                    borderColor: "#3c65a0",
+                  }}
+                  onPress={showModal}
+                >
+                  <View style={{ flexDirection: "row" }}>
+                    <Text style={{ color: "#3c65a0" }}>{item}</Text>
+                    <Text> </Text>
+                    <Ionicons
+                      name={"chevron-down-outline"}
+                      size={17.5}
+                      color={"#3c65a0"}
+                    />
+                  </View>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={showModalForCamera}>
+                  <Ionicons
+                    name="add-circle-outline"
+                    color="#3c65a0"
+                    style={{ marginLeft: 10, marginTop: 1.5 }}
+                    size={25}
+                  />
+                </TouchableOpacity>
+              </View>
+              <TextInput
+                style={{ height: 200, marginTop: 10 }}
+                multiline
+                placeholder="Something cooking?"
+              ></TextInput>
+            </View>
           </View>
         </View>
-      </SafeAreaView>
-    </TouchableWithoutFeedback>
+      </TouchableWithoutFeedback>
+    </Provider>
   );
 };
 
